53ec6108f13e135cdb7b87a135de8ddee81d44af
Leah <101liaria101@gmail.com>
2025-08-19 20:11:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import random
+
 letters = "abcdefgilmnoprstuwy"
 chars = letters + letters.upper() + "013579" + "-_~"
+
 length = int(input("Enter password length: "))
 
 print("\nCUTE PASSWORDS")
 print("*" * (length + 10))
+
 for i in range(3):
     password = "".join(random.choices(chars, k=length))
     print("Password", i+1, ":", password)
+
 print("*" * (length + 10))
+
+messages = [
+    "Stay safe online!",
+    "Keep your passwords secret!",
+    "Secure is cute!",
+    "Your data deserves love!"
+]
+print(random.choice(messages))
